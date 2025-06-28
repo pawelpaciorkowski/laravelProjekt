abd4107351991618d26868d9fb1b71dfece1c6a6
Pawel Paciorkowski <pawel.paciorkowski21@gmail.com>
2025-06-28 17:55:42 +0200

    update

--- resources/views/tags/index.blade.php
+++ resources/views/tags/index.blade.php
@@ -19,11 +19,9 @@
                     <div class="flex items-center justify-between mb-6">
                         {{-- Przycisk dodawania widoczny tylko dla moderatorów i adminów --}}
                         @if(in_array(auth()->user()->role, ['admin', 'moderator']))
-                        <a href="{{ route('tags.create') }}" class="inline-flex items-center px-4 py-2 bg-gray-800 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-gray-700">
+                        <a href="{{ route('tags.create') }}" class="inline-flex items-center px-4 py-2 bg-gray-800 dark:bg-gray-200 border border-transparent rounded-md font-semibold text-xs text-white dark:text-gray-800 uppercase tracking-widest hover:bg-gray-700 dark:hover:bg-white focus:bg-gray-700 dark:focus:bg-white active:bg-gray-900 dark:active:bg-gray-300 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 dark:focus:ring-offset-gray-800 transition ease-in-out duration-150">
                             Dodaj Tag
                         </a>
-                        @else
-                        <div></div> {{-- Pusty div dla zachowania układu --}}
                         @endif
                         <form action="{{ route('tags.index') }}" method="GET">
                             <x-text-input type="text" name="search" placeholder="Szukaj tagów..." value="{{ request('search') }}" />
