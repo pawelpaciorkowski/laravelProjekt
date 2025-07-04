3c2d2745da6f61f3ca8ee75f617225d868b55cb4
Pawel Paciorkowski <pawel.paciorkowski21@gmail.com>
2025-07-04 14:21:50 +0200

    update

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,10 +2,8 @@ import './bootstrap';
 
 import Alpine from 'alpinejs';
 
-// === POCZĄTEK DODANEGO KODU ===
 import confetti from 'canvas-confetti';
 window.confetti = confetti;
-// === KONIEC DODANEGO KODU ===
 
 window.Alpine = Alpine;
 
